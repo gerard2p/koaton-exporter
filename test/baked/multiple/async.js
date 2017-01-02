d5144527016f92127b85c7fd91ad316c44c81503
Gerardo Pérez <gerard2perez@outlook.com>
2017-01-02 17:22:26 -0600

    test(multiple transpile): travis ci saying thay where not the same

--- test/baked/multiple/async.js
+++ test/baked/multiple/async.js
@@ -3,29 +3,29 @@
 const koa = require('koa').default;
 
 function* koaStyle(next) {
-  yield next;
+	yield next;
 }
 function* koaStyle2() {
-  yield global.next();
+	yield global.next();
 }
 const koaStyle3 = function* (next) {
-  yield next;
+	yield next;
 };
 
 koa.use(function* () {
-  this.body = 'Hello Koa';
+	this.body = 'Hello Koa';
 });
 koa.use(function* (next) {
-  const start = new Date();
-  yield next;
+	const start = new Date();
+	yield next;
 
-  const ms = new Date() - start;
-  console.log(`${ this.method } ${ this.url } - ${ ms }ms`);
+	const ms = new Date() - start;
+	console.log(`${ this.method } ${ this.url } - ${ ms }ms`);
 });
 
 koa.use(function* (next) {
-  const start = new Date();
-  yield next;0;
-  const ms = new Date() - start;
-  console.log(`${ this.method } ${ this.url } - ${ ms }ms`);
+	const start = new Date();
+	yield next;
+	const ms = new Date() - start;
+	console.log(`${ this.method } ${ this.url } - ${ ms }ms`);
 });
