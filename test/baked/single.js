416356d04b15d3fae7e3d0ab7cd68d11eb7f64bb
Gerardo Pérez Pérez <gerard2perez@outlook.com>
2017-01-14 18:36:36 -0600

    test: added test to no transpile normal functions

--- test/baked/single.js
+++ test/baked/single.js
@@ -1,10 +1,12 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 function* async1(next) {
-    yield next;
+  yield next;
 }
-
+function noasync(dat1, dat2) {
+  console.log(dat1);
+}
 exports.default = async1;
